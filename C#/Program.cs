49d3a8d26b006f2505b60a12e0677168a1844ce3
VMPYRC <90119648+VMPYRC@users.noreply.github.com>
2023-05-15 09:01:58 -0400

    C# Variables

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,9 +1,38 @@
 ﻿using System;
 
-namespace HelloWorld{
-	class Program{
-		static void Main(string[] args){
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
 			Console.WriteLine("Hello World!");
+
+			char x = 'x'; // character
+			Console.WriteLine(x);
+
+			String delicious = " is delicious."; // string
+			string food = "Takoyaki";
+			Console.WriteLine(food + delicious);
+			food = "Onigiri"; // changed value
+			Console.WriteLine(food + delicious);
+
+			bool trueBool = true; // boolean
+
+			int one = 1; // integer, whole number
+			int two; // declared
+			two = 2; // assigned later
+			const int three = 3; // const = unchangeable
+			int four = 4, five = 5, six = 6;
+			int sevn, svn, seven;
+			sevn = svn = seven = 7;
+			Console.WriteLine(one + two + three + four + five + six);
+			Console.WriteLine(sevn + svn + seven);
+
+			double eightTwelve = 8.12D; // floating point number
+			Console.WriteLine(eightTwelve);
+
+
 		}
 	}
 }
